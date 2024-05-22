66a3427a37ff16924e09025a26b1ae7f044a007f
Julius <julius.plass@hotmail.com>
2024-05-22 13:48:31 +0200

    * Added ESLint plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// @ts-expect-error TS7016
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    eslint({
+      emitError: true,
+      emitWarning: true,
+      failOnError: false,
+      failOnWarning: false,
+    }),
+  ],
 });
